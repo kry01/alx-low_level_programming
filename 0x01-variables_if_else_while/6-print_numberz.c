bd96804a7f12169713b99963ce836865e8dff60c
kry01 <brahimsecond@gmail.com>
2023-03-17 14:00:00 -0700

    add tasks files for 6 task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this program is to know what is the sign of n.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
