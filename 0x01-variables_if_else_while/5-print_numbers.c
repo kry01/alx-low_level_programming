af0f3914a187401f78e0387219ba373f211d09e2
kry01 <brahimsecond@gmail.com>
2023-03-17 13:31:01 -0700

    add files for a 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this program is to know what is the sign of n.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
+
