d61b2e90b28a7d43e4637aaa12f31d848ab75290
kry01 <brahimsecond@gmail.com>
2023-03-17 10:52:40 -0700

    add 0-positive_or_negative.c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main : this program is to know what is the sign of n.
+ * @n : random number
+ * return : 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+		printf("is negative\n");
+	else if (n = 0)
+		printf("is zero\n");
+	else
+		printf("is positive\n");
+	return (0);
+}
+
