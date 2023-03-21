fb93e4ccfc32a72aaed5c0920e0ce956d200be00
kry01 <brahimsecond@gmail.com>
2023-03-21 16:48:13 -0700

    modified

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 /**
  * main - the principal function
- * @i: var for loop
- * @sum: sum for loop
  * Return: 0
  */
 int main(void)
@@ -11,11 +9,11 @@ int main(void)
 
 	for (i = 1; i <= 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0 )
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
 			sum += i;
 		}
 	}
 	printf("%d\n", sum);
-	return(0);
+	return (0);
 }
