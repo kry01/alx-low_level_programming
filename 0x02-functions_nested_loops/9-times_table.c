f9a03b1cbe163f88c26680edfed43e16c479d911
kry01 <brahimsecond@gmail.com>
2023-03-21 14:59:48 -0700

    add files for 9's task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the time from 00:00 to 23:59
+ **/
+void times_table(void)
+{
+	int i, j, mult;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(0 + '0');
+		for (j = 1; j < 10; j++)
+		{
+			mult = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (mult <= 9)
+			{
+				_putchar(' ');
+				_putchar(mult + '0');
+			}
+			else
+			{
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			}
+		}
+		_putchar('\n');
+
+	}
+}
