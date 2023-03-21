778305b75013ae4f4ae0a00408974987777a51f7
kry01 <brahimsecond@gmail.com>
2023-03-21 10:49:58 -0700

    uploaded

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 				_putchar(i + '0');
 			}	
 			else
-				_putchar(i + '0');
+				_putchar(i);
 			_putchar(':');
 			if (j < 10)
 			{
@@ -26,7 +26,7 @@ void jack_bauer(void)
 				_putchar(j + '0');
 			}
 			else
-				_putchar(j + '0');
+				_putchar(j);
 			_putchar('\n');
 		}
 	}
