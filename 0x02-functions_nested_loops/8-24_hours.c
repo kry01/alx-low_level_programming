b585d4dec413881506a324b8527afa770d77033d
kry01 <brahimsecond@gmail.com>
2023-03-21 10:44:40 -0700

    add some filse for 8's task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - print the last digit of a number
+ * @i: var int.
+ * @j: another int
+ * Return: return 0.
+ */
+void jack_bauer(void)
+{
+	int i, j;
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			if (i < 10)
+			{
+				_putchar(0 + '0');
+				_putchar(i + '0');
+			}	
+			else
+				_putchar(i + '0');
+			_putchar(':');
+			if (j < 10)
+				_putchar(0 + '0');
+				_putchar(j + '0');
+			else
+				_putchar(j + '0');
+		}
+	}
+}
