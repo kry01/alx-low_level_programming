a41058b435c6bed3df3a5ce9979595787c1506d6
kry01 <brahimsecond@gmail.com>
2023-03-21 10:03:59 -0700

    add 6-abs.c files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check is greater than 0 or not.
+ * @n: var for char.
+ * Return: return 0.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
