63086b161e138bf5b15c4750caa9422d8ab7c960
kry01 <brahimsecond@gmail.com>
2023-03-21 09:51:52 -0700

    add some files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - check is greater than 0 or not.
+ * @n: var for char.
+ * Return: return 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
