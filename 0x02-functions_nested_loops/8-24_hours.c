c38110c65c78a4d72ba583429ce5d6ed645e968c
kry01 <brahimsecond@gmail.com>
2023-03-21 11:38:21 -0700

    add files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,6 +8,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 23; i++)
 	{
 		for (j = 0; j <= 59; j++)
@@ -16,11 +17,11 @@ void jack_bauer(void)
 			{
 				_putchar(0 + '0');
 				_putchar(i + '0');
-			}	
+			}
 			else
 			{
-				_putchar(i/10 + '0');
-				_putchar(i%10 + '0');
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
 			}
 			_putchar(':');
 			if (j < 10)
@@ -30,8 +31,8 @@ void jack_bauer(void)
 			}
 			else
 			{
-				_putchar(j/10 + '0');
-				_putchar(j%10 + '0');
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 			}
 			_putchar('\n');
 		}
