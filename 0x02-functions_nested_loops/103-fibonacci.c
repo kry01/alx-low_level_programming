064ba5cf59941e6a753c7ecefb43b679bbb35b22
kry01 <brahimsecond@gmail.com>
2023-03-21 17:45:50 -0700

    add files for task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - the principal function
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 0;
+	unsigned long int sum, var1, var2, final;
+
+	var1 = 1;
+	var2 = 2;
+	final = sum = 0;
+	while (sum <= 4000000)
+	{
+			sum = var1 + var2;
+			var1 = var2;
+			var2 = sum;
+			if (var1 % 2 == 0)
+				final = final + var1;
+	}
+	printf("%ld\n", final);
+	return (0);
+}
