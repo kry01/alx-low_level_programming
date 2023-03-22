c56c30c48c0f111ad7996a33ac68b9c5adaac5cb
kry01 <brahimsecond@gmail.com>
2023-03-21 18:17:34 -0700

    add files for later

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - the principal function
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 0;
+	float sum, var1, var2;
+
+	var1 = 1;
+	var2 = 2;
+	printf("%0.f, %0.f, ", var1, var2);
+	while (count < 96)
+	{
+		sum = var1 + var2;
+		printf("%0.f", sum);
+		if (count == 95)
+			break;
+		printf(", ");
+		var1 = var2;
+		var2 = sum;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
