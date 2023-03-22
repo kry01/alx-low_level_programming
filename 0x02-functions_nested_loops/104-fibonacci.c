e9d32debd1aeca47714511b2b1f7a473c80e9173
kry01 <brahimsecond@gmail.com>
2023-03-21 18:29:02 -0700

    add files

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,15 +6,15 @@
 int main(void)
 {
 	int count = 0;
-	double sum, var1, var2;
+	long double sum, var1, var2;
 
 	var1 = 1;
 	var2 = 2;
-	printf("%g, %g, ", var1, var2);
+	printf("%0.Lg, %0.Lg, ", var1, var2);
 	while (count < 96)
 	{
 		sum = var1 + var2;
-		printf("%g", sum);
+		printf("%0.Lg", sum);
 		if (count == 95)
 			break;
 		printf(", ");
