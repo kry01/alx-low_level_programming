2fcbe18f52dee1da2f5a80cad89d6f288b318d57
kry01 <brahimsecond@gmail.com>
2023-03-23 10:49:51 -0700

    upadted

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,13 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('\\');
-		_putchar('\n');
-		for (j = 0; j < i + 1; j++)
+		if (i > 0)
+			_putchar('\n');
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
+		_putchar('\\');
 	}
 	_putchar('\n');
 }
