e8e8b1d6173efe74bcfef68811a3760c2434f8e5
kry01 <brahimsecond@gmail.com>
2023-03-23 09:19:07 -0700

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
-/*
- *  more_numbers - add more numbers.
+/**
+ * more_numbers - add more numbers from 0 to 14..
  *
- *  Return : 0 and 1.
  */
 
 void more_numbers(void)
 {
 	int i, j;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
