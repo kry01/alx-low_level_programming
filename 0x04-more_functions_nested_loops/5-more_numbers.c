da5d050d4d7cf1de7752f81f8cfec9e18e2a7af1
kry01 <brahimsecond@gmail.com>
2023-03-23 09:15:32 -0700

    add files for task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ *  more_numbers - add more numbers.
+ *
+ *  Return : 0 and 1.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
