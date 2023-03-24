5d0a207cb7b9651f16979650c5985d71a8fc5ab0
kry01 <brahimsecond@gmail.com>
2023-03-24 11:14:12 -0700

    add files for task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function that print numbers.
+ *
+ * @n: the number.
+ *
+ */
+
+void print_number(int n)
+{
+	int num = n;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
+}
