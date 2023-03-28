0ff58e779699df03cdb12e9ed3bbb29366a947e9
kry01 <brahimsecond@gmail.com>
2023-03-27 19:34:04 -0700

    add files for task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - a function.
+ *
+ *@str: string.
+ *
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	if (count % 2 != 0)
+		count--;
+	for (i = count / 2; i <= count; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
