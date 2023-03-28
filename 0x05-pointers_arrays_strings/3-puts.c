ea53e846bbd61676819e08b8672d1dc2881dcc27
kry01 <brahimsecond@gmail.com>
2023-03-27 17:50:20 -0700

    add files for task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - print the string.
+ *
+ *@c: char.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+		i++;
+	}
+}
