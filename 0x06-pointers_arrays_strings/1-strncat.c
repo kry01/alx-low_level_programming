447dd318a31718a70e2349cafa22926e6bf5254c
kry01 <brahimsecond@gmail.com>
2023-03-29 09:06:53 -0700

    add files for task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - the concatnate function.
+ *
+ * @dest: first string.
+ * @src: second string.
+ * @n: number of chars.
+ *
+ * Return: String.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (i >= 0)
+	{
+		if (*(dest + i) == '\0')
+		{
+			for (j = 0; j < n; j++)
+			{
+				*(dest + i) = *(src + j);
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (dest);
+}
