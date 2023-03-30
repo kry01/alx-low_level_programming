f58520132880cef335c3d26d0ec395b65c402d41
kry01 <brahimsecond@gmail.com>
2023-03-30 16:53:02 -0700

    modofed

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,16 @@
  */
 void print_number(int n)
 {
+	int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n > 9)
-		print_number(n / 10);
-	_putchar(n % 10 + 0);
-	_putchar(10);
-
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
 }
