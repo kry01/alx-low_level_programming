eb0fcaa1dfd5570d2f33bc6fa0e0e3e43342014f
kry01 <brahimsecond@gmail.com>
2023-03-29 17:40:37 -0700

    add files for task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - the function to copy.
+ *
+ * @dest: 1st string.
+ * @src: 2nd String.
+ * @n: number of char.
+ * Return: return string.
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, cmpt = 0;
+
+	while (*(src + cmpt) != '\0')
+		cmpt++;
+	while (i < n)
+	{
+		if (i >= cmpt)
+			*(dest + i) = '\0';
+		else
+			*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
