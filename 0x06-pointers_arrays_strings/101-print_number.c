5e9f7d84f567733413d9424715eeddc238c6aa2f
kry01 <brahimsecond@gmail.com>
2023-03-30 16:42:25 -0700

    add files for task 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - function to print numbers.
+ * @n: number.
+ *
+ */
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+		print_number(n / 10);
+	_putchar(n % 10 + 0);
+	_putchar(10);
+
+}
