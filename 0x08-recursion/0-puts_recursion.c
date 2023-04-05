44dfc073769a991d6cce44c5d19fa8defcc30e51
kry01 <brahimsecond@gmail.com>
2023-04-05 09:22:34 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@
  */
 
 void _puts_recursion(char *s)
-{	
+{
 	if (*(s + 0) == '\0')
 	{
 		printf("\n");
 		return;
 	}
-	printf("%c",*(s + 0));
+	printf("%c", *(s + 0));
 	_puts_recursion(s + 1);
 }
