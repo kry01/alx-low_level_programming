1f0cec57b999d7bd904b2b0fcc2c55a4e9a02063
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-09 22:55:47 +0000

    add files for task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the multip of two numbers followed by a new line..
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
