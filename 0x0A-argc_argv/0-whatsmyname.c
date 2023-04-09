7517d2590360a759ee029198e258a98a3f4a8038
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-09 14:36:56 +0000

    add files for task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the main function.
+ * 
+ */
+int main(int __attribute__ (unused) agrc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
