2ed887b5d09e93b8ef6775c3975aa8a7da7a1334
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-10 15:29:01 -0700

    fix errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,35 +8,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int tab[] = {25, 10, 5, 2, 1};
-	int i = 0, count = 0;
-	int sum = atoi(argv[1]);
+	int cents, ncoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		if (sum < 0)
-		{
-			printf("0\n");
-		}
-		else
-		{
-			while (sum > 0)
-			{
-				if (sum >= tab[i])
-				{
-					sum = sum - tab[i];
-					count++;
-					continue;
-				}
-				i++;
-			}
-			printf("%d\n", count);
-		}
-		return (0);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
+	printf("%d\n", ncoins);
+	return (0);
 }
