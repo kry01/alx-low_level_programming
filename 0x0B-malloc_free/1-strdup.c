0d6cedb8a58e393b82709d94dd20074c9db3b713
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-11 23:23:06 +0000

    fix some changes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	while (*(str + count) != 0)
 		count++;
 	p = malloc(sizeof(char) * count + 1);
-	if (*str == 0)
+	if (*str == 0 || str == NULL)
 		return ('\0');
 	if (p == 0)
 		return ('\0');
