c3442b67c9e062538f0b1224828b2bbd60616e08
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-12 02:00:15 +0000

    add files for task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that concatenates two strings.
+ *
+ * @width: number.
+ * @height: number..
+ *
+ * Return: pointer.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return 0;
+	p = (int **)malloc(sizeof(int *) * height);
+	if (p == 0)
+		return (0);
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			*(p + i) = malloc (sizeof(int) * width);
+			*(*(p + i) + j) = 0;
+		}
+	}
+	return (p);
+}
