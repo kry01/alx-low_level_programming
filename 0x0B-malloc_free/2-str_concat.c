ddadb9feb628c3b08fb5cbca608794760b9fb54b
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-12 00:36:12 +0000

    fix some errors

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,26 @@ char *str_concat(char *s1, char *s2)
 	int i, j, c1 = 0, c2 = 0;
 	char *p;
 
-	while (*(s1 + c1) != 0)
-		c1++;
-	while (*(s2 + c2) != 0)
-		c2++;
+	if (s1 != 0)
+	{
+		while (*(s1 + c1) != 0)
+			c1++;
+	}
+	if (s2 != 0)
+	{
+		while (*(s2 + c2) != 0)
+			c2++;
+	}
 	p = malloc(c1 + c2 + 1);
 	if (p == 0)
 		return (0);
-	for (i = 0; *(s1 + i) != 0; i++)
+	for (i = 0; i < c1; i++)
+	{
 		*(p + i) = *(s1 + i);
-	for (j = 0; *(s2 + j) != 0; j++, i++)
+	}
+	for (j = 0; j < c2; j++, i++)
+	{
 		*(p + i) = *(s2 + j);
+	}
 	return (p);
 }
