3866d3c97b2fff4fc6cdf17c2db87e4c5a2d0dc6
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-17 23:41:13 +0000

    add files for task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: Define a new type struct dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+}dog_t;
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 #endif
