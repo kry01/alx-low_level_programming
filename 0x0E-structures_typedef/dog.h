668345d2e440316d92b9007ed03f185f880650cc
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-17 14:56:42 +0000

    add betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type struct dog.
+ * @name: the name of dog.
+ * @age: dog's age.
+ * @owner: the dog owner.
+ *
+ * Description: Define a new type struct dog.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
