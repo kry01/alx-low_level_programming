c23744e226d3eaadddf972aa4386f894add154f4
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-18 01:35:22 +0000

    add files for task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,12 +2,13 @@
 #include "dog.h"
 /**
  * free_dog -  function that frees dogs.
- * @d: the dog.
- * 
+ * @d: the dog. 
  */
 
 void free_dog(dog_t *d)
 {
+	if (d == 0)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);
