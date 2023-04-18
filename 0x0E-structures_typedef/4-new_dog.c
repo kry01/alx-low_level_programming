383fff0aaa4a031fade316a8f1553e91b70c80ea
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-18 01:05:06 +0000

    fix some errors

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog.
+ * @name: the name of dog.
+ * @age: the age of dog.
+ * @owner: the owner of dog.
+ *
+ * Return: retuen 0 or value
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int i, j, x;
+
+	d = malloc(sizeof(dog_t));
+	if (d == 0 || owner == 0 || name == 0)
+	{
+		free(d);
+		return (0);
+	}
+	for (i = 0; *(name + i) != 0; i++)
+		;
+	for (j = 0; *(owner + j) != 0; j++)
+		;
+
+	d->name = malloc(i + 1);
+	d->owner = malloc(j + 1);
+
+	if (d->name == 0 || d->owner == 0)
+	{
+		free(d->name);
+		free(d->owner);
+		return (0);
+	}
+	for (x = 0; x <= i; x++)
+		(*d).name[x] = *(name + x);
+	for (x = 0; x <= j; x++)
+		(*d).owner[x] = *(owner + x);
+	d->age = age;
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,5 @@ typedef struct dog
 } dog_t;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
