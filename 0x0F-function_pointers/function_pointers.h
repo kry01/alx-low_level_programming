2cac0a22d34facc62a361033e10f3ca749c23748
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-19 15:34:57 +0000

    fix some errors end add some labreries in header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
-#include <stddef.h>
 #ifndef function_pointers_h
 #define function_pointers_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
