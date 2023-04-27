030279f83acd8abfad452711f3eae1734e7aa4f1
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-27 16:13:53 +0000

    add files for task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: list to print.
+ *
+ * Return: number of nudes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	while (h != 0)
+	{
+		if (h->str == 0)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
