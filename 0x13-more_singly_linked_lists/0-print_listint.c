19351afd25a6b01a10a3811b13aeaf0a60306d61
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-04-28 22:46:53 +0000

    add files for task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: nude.
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	
+}
