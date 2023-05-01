78f0c37b68ba88f5569a99130a78449018b7afb6
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-05-01 12:03:24 +0100

    edit files for task 4 and task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a list.
+ * @head: head node of the list.
+ * @index: index of the node which to delete.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *node;
+	unsigned int i;
+
+	temp = malloc(sizeof(listint_t));
+	temp = *head;
+	if (temp == 0)
+		return (0);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	for (i = 0; i < index - 1; i++)
+		temp = temp->next;
+	node =  temp->next;
+	temp->next = node->next;
+	free(node);
+	return (1);
+}
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,7 +10,8 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	temp = head;
+	while (temp != NULL)
 	{
 		temp = head;
 		head = head->next;
