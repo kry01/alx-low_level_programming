4a06272015286df8f148fe30838323633d8d21f1
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-05-08 21:21:26 +0100

    add files for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,11 +19,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	o = open(filename, O_RDONLY);
 	if (o == -1)
-	{
-		free(bf);
 		return (0);
-	}
 	r = read(o, bf, letters);
+	bf[letters] = '\0';
 	w = write(1, bf, r);
 	free(bf);
 	close(o);
