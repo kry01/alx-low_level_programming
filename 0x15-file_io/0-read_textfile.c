d17fe4bd0cb11270f34d2b63b28fc54fcb0abe95
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-05-08 21:08:51 +0100

    add files for task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads atext file and prints it to the POSIX standard output.
+ * @filename: name of file.
+ * @letters: 
+ * Return: the actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *bf;
+	int o, r, w;
+
+	if (filename == 0)
+		return (0);
+	bf = malloc(letters);
+	if (bf == 0)
+		return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, bf, letters);
+	w = write(1, bf, r);
+	free(bf);
+	close(o);
+	return (w);
+}
