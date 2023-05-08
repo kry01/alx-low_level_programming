d495b2d79e34791be771f44d54c2f62876763420
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-05-08 22:26:25 +0100

    add files for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: file name.
+ * @text_content: content as text.
+ *
+ * Return 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, w;
+
+	if (filename == 0)
+		return (0);
+
+	o = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (o == -1)
+		return (-1);
+	if (text_content != 0)
+		w = write(o, text_content, strlen(text_content));
+	else
+		w = write(o, "", 0);
+	if (w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
